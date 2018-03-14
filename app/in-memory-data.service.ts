fd2e713c8e4c1518a9a91e211a5496ce3b7a3241
Ali Lamine <lamineali719@gmail.com>
2018-03-14 10:06:58 +0000

    Dossier src

---
+++ app/in-memory-data.service.ts
@@ -0,0 +1,19 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 25, name: 'Lave-linge' },
+      { id: 26, name: 'Sèche-linge' },
+      { id: 27, name: 'Lave-vaisselle' },
+      { id: 28, name: 'Réfrigérateur' },
+      { id: 29, name: 'Congélateur' },
+      { id: 30, name: 'Four' },
+      { id: 31, name: 'Cuisinière' },
+      { id: 32, name: 'Micro-ondes ' },
+      { id: 33, name: 'Mini-four' },
+      { id: 34, name: 'Hotte' }
+    ];
+    return {heroes};
+  }
+}
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 25, name: 'Lave-linge' },
+  { id: 26, name: 'Sèche-linge' },
+  { id: 27, name: 'Lave-vaisselle' },
+  { id: 28, name: 'Réfrigérateur' },
+  { id: 29, name: 'Congélateur' },
+  { id: 30, name: 'Four' },
+  { id: 31, name: 'Cuisinière' },
+  { id: 32, name: 'Micro-ondes ' },
+  { id: 33, name: 'Mini-four' },
+  { id: 34, name: 'Hotte' }
+];
